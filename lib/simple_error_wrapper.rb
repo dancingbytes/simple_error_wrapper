2dc128dfb6d10aa24b8c4ead11f0678a7c2b14d6
Tyralion <piliaiev@gmail.com>
2017-10-23 11:07:47 +0500

    Версия рельсы берется из константы

--- lib/simple_error_wrapper.rb
+++ lib/simple_error_wrapper.rb
@@ -1,20 +1,18 @@
 # encoding: utf-8
 require 'simple_error_wrapper/base'
 
-if defined?(::Bootstrap)
-  require 'simple_error_wrapper/bootstrap'
-elsif defined?(::Materialize)
+# if defined?(::Bootstrap)
+#  require 'simple_error_wrapper/bootstrap'
+if defined?(::Materialize)
   require 'simple_error_wrapper/materialize'
 else
   require 'simple_error_wrapper/default'
 end
 
-ver, maj, min = Rails.version.split(".").map!(&:to_i)
-
 if defined?(ActionView)
 
   ActionView::Base.send :include, ::SimpleErrorWrapper::ActionViewBase
-  if ver < 4
+  if Rails::VERSION::MAJOR < 4
     ActionView::Helpers::InstanceTag.send :include, ::SimpleErrorWrapper::HelpersInstanceTag
   else
     ActionView::Helpers::Tags::Base.send  :include, ::SimpleErrorWrapper::HelpersInstanceTag
